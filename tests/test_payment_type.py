d9aa7d7e5c72d26d217c962598c144486e502ac8
Brandon De Leon <brandondeleon.615@gmail.com>
2021-11-30 14:50:38 -0600

    added a payment type test

--- tests/test_payment_type.py
+++ tests/test_payment_type.py
@@ -5,13 +5,15 @@ from rest_framework.authtoken.models import Token
 from django.core.management import call_command
 from django.contrib.auth.models import User
 
+from bangazon_api.models import payment_type, PaymentType
+
 
 class PaymentTests(APITestCase):
     def setUp(self):
         """
         Seed the database
         """
-        call_command('seed_db', user_count=1)
+        call_command('seed_db', user_count=2)
         self.user1 = User.objects.filter(store=None).first()
         self.token = Token.objects.get(user=self.user1)
 
@@ -20,6 +22,15 @@ class PaymentTests(APITestCase):
 
         self.faker = Faker()
 
+        payment_type = {
+            "merchant": "Discover",
+            "acctNumber": "123344566",
+            "customer": 1
+        }
+
+        response = self.client.post(
+            f'/api/payment-types', payment_type, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_create_payment_type(self):
         """
@@ -37,3 +48,21 @@ class PaymentTests(APITestCase):
         self.assertIsNotNone(response.data['id'])
         self.assertEqual(response.data["merchant_name"], data['merchant'])
         self.assertEqual(response.data["acct_number"], data['acctNumber'])
+
+    def test_delete_payment_type(self):
+        """
+       Ensure we can delete a paymentType.
+       """
+        payment_type = PaymentType.objects.first()
+        data = {
+            "merchant_name": payment_type.merchant_name,
+            "acct_number": payment_type.acct_number,
+            "customer": payment_type.customer
+
+        }
+        response = self.client.delete(
+            f'/api/payment-types/{payment_type.id}', data)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+        response = self.client.delete(f'/api/payment-types/{payment_type.id}')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
